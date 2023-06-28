07c3ff09b555da9877bcf5d24e80d93e761d9ad8
Gabriel Giovani <gabrielgiovanesouza32@gmail.com>
2023-06-28 14:54:36 -0300

    Operacao de instanciacao da base de dados

--- src/main/java/com/nelioalves/workshopmongo/domain/User.java
+++ src/main/java/com/nelioalves/workshopmongo/domain/User.java
@@ -6,7 +6,7 @@ import java.util.Objects;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document
+@Document(collection = "user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
