9969f06aafad5d019095e46be057cfab49931af0
Gabriel Giovani <gabrielgiovanesouza32@gmail.com>
2023-06-30 16:56:17 -0300

    Consulta simples com @Query

--- src/main/java/com/nelioalves/workshopmongo/services/PostService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/PostService.java
@@ -25,6 +25,6 @@ public class PostService {
 	}
 	
 	public List<Post> findByTitle(String text){
-		return postRepository.findByTitleContainingIgnoreCase(text);
+		return postRepository.searchTitle(text);
 	}
 }
